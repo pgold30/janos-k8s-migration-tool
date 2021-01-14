ae3f9610c567ad30f3192f98e0ca88857f3fe1a6
pablo <loschi.pablo@gmail.com>
2021-01-14 14:02:45 +0100

    clean version

--- janos.sh
+++ janos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Version: 0.3
+# Version: 0.4
 # Maintainer: @pgold30
-# Repo: https://github.com/pgold30/k8s-transmogrifier
+# Repo: https://github.com/pgold30/janos-k8s-migration-tool/
 
 #SEARCHPATH=$1
 echo "insert full path where yamls files exist"
@@ -16,8 +16,8 @@ usage () {
   echo "---------------------------------------------------------------"
   echo "Error: $errorMessage"
   echo "---------------------------------------------------------------"
-  echo "usage: ./transmogrify_for_k8s_1.16.sh <path to manifests>"
-  echo "  e.g. ./transmogrify_for_k8s_1.16.sh /tmp/my-yamls"
+  echo "usage: ./janos.sh <path to manifests>"
+  echo "  e.g. ./janos.sh /tmp/my-yamls"
   echo "---------------------------------------------------------------"
   exit 1
 }
@@ -54,13 +54,12 @@ function add_spec-selector () {
   yq -i eval '.spec.selector.matchLabels.app |= env(METADATANAME)' $FILE
 }
 
-
+# This funtion will validate the resulting file for the given K8s version
 function validate {
   FILE=$1
   cat $FILE | kubeval --kubernetes-version $kubeversion
 }
 
-#for file in $SEARCHPATH/*.yaml
 for file in $(find $SEARCHPATH -name '*.yaml' -or -name '*.yml');
 do
   echo "1:Replacing all deprecated APIs for K8s 1.16..."
@@ -70,5 +69,3 @@ do
   echo "3:Validating file for K8s specified version with kubeval..."
   validate $file
 done
-
-#step a= #find $target -iname "*.yaml" -exec ./transmogrify_for_k8s_1.16.sh {} \;
